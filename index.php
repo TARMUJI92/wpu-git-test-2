cc751838af4fbc57ca1b61e3854a8a73a8adcc67
TARMUJI92 <tarmuji594@gmail.com>
2025-08-11 14:17:22 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
         <H1>HIT REMOTE LOACAL
         </H1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto blanditiis veniam quia reprehenderit veritatis molestiae quibusdam. Nesciunt voluptas dolorum assumenda earum molestiae rerum repudiandae totam enim porro quam, ea ex.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis hic, commodi esse facere numquam, soluta molestias voluptatum repudiandae temporibus voluptatem dignissimos est quia tenetur et vel similique labore explicabo ullam.</p>
 </body>
 </html>
